f688b40b1a90f5e42c57774493e60b5d22d8ffaa
Zachary Zulanas <zzulanas@gmail.com>
2020-02-22 12:49:33 -0800

    timeline added and base of student page

--- src/views/pages/Students.jsx
+++ src/views/pages/Students.jsx
@@ -21,19 +21,19 @@ import classnames from "classnames";
 
 // reactstrap components
 import {
-  Badge,
-  Button,
-  Card,
-  CardBody,
-  CardImg,
-  FormGroup,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  Container,
-  Row,
-  Col
+    Badge,
+    Button,
+    Card,
+    CardBody,
+    CardImg,
+    FormGroup,
+    Input,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroup,
+    Container,
+    Row,
+    Col
 } from "reactstrap";
 
 // core components
@@ -43,18 +43,129 @@ import CardsFooter from "components/Footers/CardsFooter.jsx";
 import Download from "../IndexSections/Download.jsx";
 import DefaultNavbar from "views/IndexSections/DefaultNavbar.jsx";
 import StudentHero from "components/StudentHero.jsx";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import 'react-vertical-timeline-component/style.min.css';
 
-class Students extends React.Component{
+
+class Students extends React.Component {
     state = {};
     componentDidMount() {
         document.documentElement.scrollTop = 0;
         document.scrollingElement.scrollTop = 0;
-      }
-    render(){
-        return(
+    }
+    render() {
+        return (
             <>
-                <DefaultNavbar/>
-                <StudentHero/>
+                <DefaultNavbar />
+                <StudentHero />
+                <section className="section section-shaped">
+                    <Container>
+                        <Row>
+                            <Col className="col-sm">
+                                <h1 className="h1">
+                                    Interested in joining Gesher?
+                                </h1>
+                                <p>
+                                    Check out our timeline below to see when we're recruiting. We recruit mainly in Fall and Spring quarters, so check back at the beginning of either quarter to apply.
+                                </p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
+                <section className="section section-shaped">
+                    <VerticalTimeline>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            contentStyle={{  color: '#fff' }}
+                            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                            date="3/08/2020"
+                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                            icon={''}
+                        >
+                            <h3 className="vertical-timeline-element-title">Applications Open</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                            <p>
+                                Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+    </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date="4/02/2020"
+                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                            icon={'src/assets/img/brand/GesherStack1.svg'}
+                        >
+                            <h3 className="vertical-timeline-element-title">Art Director</h3>
+                            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                            <p>
+                                Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+    </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date="2008 - 2010"
+                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                            icon={''}
+                        >
+                            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                            <p>
+                                User Experience, Visual Design
+    </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date="2006 - 2008"
+                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                            icon={'src/assets/img/brand/GesherStack1.svg'}
+                        >
+                            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                            <p>
+                                User Experience, Visual Design
+    </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            date="April 2013"
+                            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                            icon={'src/assets/img/brand/GesherStack1.svg'}
+                        >
+                            <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+                            <p>
+                                Strategy, Social Media
+    </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            date="November 2012"
+                            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                            icon={'src/assets/img/brand/GesherStack1.svg'}
+                        >
+                            <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+                            <p>
+                                Creative Direction, User Experience, Visual Design
+    </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            date="2002 - 2006"
+                            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                            icon={'src/assets/img/brand/GesherStack1.svg'}
+                        >
+                            <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+                            <p>
+                                Creative Direction, Visual Design
+    </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                            icon={'src/assets/img/brand/GesherStack1.svg'}
+                        />
+                    </VerticalTimeline>
+                </section>
             </>
         )
     }
