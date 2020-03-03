7272bf866abc853eda17077b72e76eff6636d84f
Zachary Zulanas <zzulanas@gmail.com>
2020-03-03 14:48:39 -0800

    added content for modals

--- src/views/IndexSections/Services.jsx
+++ src/views/IndexSections/Services.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 // reactstrap components
-import { Button, Container, Row, Col, Card, CardBody, Badge } from "reactstrap";
+import { Button, Container, Row, Col, Card, CardBody, Badge, Modal } from "reactstrap";
 
 class Services extends React.Component {
+  state = {
+    bizDevModal: false,
+    markModal: false,
+    dataModal: false,
+  }
+  toggleModal = state => {
+    this.setState({
+        [state] : !this.state[state]
+    });
+  };
   render() {
     return (
       <>
@@ -54,12 +64,54 @@ class Services extends React.Component {
                           className="mt-4"
                           color="primary"
                           href="#pablo"
-                          onClick={e => e.preventDefault()}
+                          onClick={() => this.toggleModal("bizDevModal")}
                         >
                           Learn more
                         </Button>
                       </CardBody>
                     </Card>
+                    <Modal
+              className="modal-dialog-centered"
+              isOpen={this.state.bizDevModal}
+              toggle={() => this.toggleModal("bizDevModal")}
+            >
+              <div className="modal-header">
+                <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
+                  <i className="ni ni-briefcase-24" />
+                </div>
+                <h6 className="modal-title text-primary text-uppercase" id="modal-title-default" style={{marginLeft:"10px", marginTop: "18px"}}>
+                  Business Development
+                </h6>
+                <button
+                  aria-label="Close"
+                  className="close"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("bizDevModal")}
+                >
+                  <span aria-hidden={true}>×</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <p>
+                    At it's heart, Gesher Group is a business-focused organization. We strive to help you achieve your goals.
+                </p>
+                <p>
+                    A majority of our members have had real world experience working in business roles where they've been able to analyze company workflows and propose improvements to save time and money. Our members have worked with many smaller businesses and helped them grow into larger entities to accomplish their goals.
+                </p>
+              </div>
+              <div className="modal-footer">
+                <Button
+                  className="ml-auto"
+                  color="link"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("bizDevModal")}
+                >
+                  Close
+                </Button>
+              </div>
+            </Modal>
                   </Col>
                   <Col lg="4">
                     <Card className="card-lift--hover shadow border-0">
@@ -77,12 +129,54 @@ class Services extends React.Component {
                           className="mt-4"
                           color="warning"
                           href="#pablo"
-                          onClick={e => e.preventDefault()}
+                          onClick={() => this.toggleModal("markModal")}
                         >
                           Learn more
                         </Button>
                       </CardBody>
                     </Card>
+            <Modal
+              className="modal-dialog-centered"
+              isOpen={this.state.markModal}
+              toggle={() => this.toggleModal("markModal")}
+            >
+              <div className="modal-header">
+              <div className="icon icon-shape icon-shape-warning rounded-circle mb-4">
+                          <i className="ni ni-laptop" />
+              </div>
+                <h6 className="modal-title text-warning text-uppercase" id="modal-title-default" style={{marginLeft:"10px", marginTop: "18px"}}>
+                  Digital Marketing
+                </h6>
+                <button
+                  aria-label="Close"
+                  className="close"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("markModal")}
+                >
+                  <span aria-hidden={true}>×</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <p>
+                    Our digital marketing division of Gesher is devoted to building massive online audiences that translate into direct sales.
+                </p>
+                <p>
+                    Gesher Group specializes in building brands up through social media and creating original content. We have many consultants who are well versed in professional branding principles and who have track records of building audiences with thousands of followers from the ground up.
+                </p>
+              </div>
+              <div className="modal-footer">
+                <Button
+                  className="ml-auto"
+                  color="link"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("markModal")}
+                >
+                  Close
+                </Button>
+              </div>
+            </Modal>
                   </Col>
                   <Col lg="4">
                     <Card className="card-lift--hover shadow border-0">
@@ -100,12 +194,62 @@ class Services extends React.Component {
                           className="mt-4"
                           color="success"
                           href="#pablo"
-                          onClick={e => e.preventDefault()}
+                          onClick={() => this.toggleModal("dataModal")}
                         >
                           Learn more
                         </Button>
                       </CardBody>
                     </Card>
+                    <Modal
+              className="modal-dialog-centered"
+              isOpen={this.state.dataModal}
+              toggle={() => this.toggleModal("dataModal")}
+            >
+              <div className="modal-header">
+                <div className="icon icon-shape icon-shape-success rounded-circle mb-4">
+                  <i className="ni ni-chart-bar-32" />
+                </div>
+                <h6 className="modal-title text-success text-uppercase" id="modal-title-default" style={{marginLeft:"10px", marginTop: "18px"}}>
+                  Data Analytics
+                </h6>
+                <button
+                  aria-label="Close"
+                  className="close"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("dataModal")}
+                >
+                  <span aria-hidden={true}>×</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <p>
+                    Our Data Analytics team resides under our "Labs" division. Our Labs team focuses mainly on integrating
+                    technology into our workflow and our toolbelt for each consultant. We focus on learning how to build reports using real world data, and we focus on learning Data Science principles to help companies improve their business strategies.
+                </p>
+                <p>
+                    Each quarter labs members will learn from a variety of skills including:
+                    <ul>
+                    <li>Python & SciPy libraries</li>
+                    <li>Introductory Machine Learning</li>
+                    <li>Tableau</li>
+                    <li>SQL</li>
+                    <li>Google Analytics</li>
+                    </ul>
+                </p>
+              </div>
+              <div className="modal-footer">
+                <Button
+                  className="ml-auto"
+                  color="link"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("dataModal")}
+                >
+                  Close
+                </Button>
+              </div>
+            </Modal>
                   </Col>
                 </Row>
               </Col>
